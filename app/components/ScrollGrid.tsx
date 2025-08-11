d563204b915bf70ff0b81b23de1a70ceab74e8b7
parsamoloody <parsaprogramerjs@gmail.com>
2025-08-11 18:42:01 +0330

    fssf

--- app/components/ScrollGrid.tsx
+++ app/components/ScrollGrid.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,13 +7,17 @@ import { ProductCardProps } from "@/types/type";
 import ProductCard from "./ProductCard";
 import Link from "next/link";
 
-
 const ScrollGrid = ({ data }: { data: ProductCardProps[] }) => {
   const [width, setWidth] = useState(0);
   const [preview, setPreview] = useState(3);
   const [spaceBetween, setSpaceBetween] = useState(20);
-  const products = data
+  const [domain, setDomain] = useState('');  // <--- domain in state
+
   useEffect(() => {
+    // Set domain only on client
+    setDomain(window.location.origin);
+
+    // Resize listener
     const handleResize = () => setWidth(window.innerWidth);
     handleResize();
     window.addEventListener("resize", handleResize);
@@ -42,23 +46,15 @@ const ScrollGrid = ({ data }: { data: ProductCardProps[] }) => {
     }
   }, [width]);
 
-  const domain = window.location.origin // e.g. https://example.com
-  const targetUrl = `${domain}/product/`
+  // If domain is not ready yet, don't render to avoid broken href
+  if (!domain) return null;
 
-  
   return (
     <div className="story_container my-4 px-2 lg:px-6">
-      <Swiper spaceBetween={2}
-        slidesPerView={1}
-        width={210}
-
-      >
-        {products.map((a, i) => (
+      <Swiper spaceBetween={spaceBetween} slidesPerView={preview} width={210}>
+        {data.map((a, i) => (
           <SwiperSlide key={i}>
-            <Link
-              href={{
-                pathname: targetUrl + a.id.toLocaleLowerCase(),
-              }}>
+            <Link href={`${domain}/product/${a.id.toLowerCase()}`}>
               <ProductCard
                 id={a.id}
                 title={a.title}
